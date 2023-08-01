3162e9024622b5977d9f1d7f10c71947df28d22c
tsuesun <solomon.redfern@tradeshift.com>
2023-08-01 10:37:28 +0900

    Remove proxy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import {setFailed} from '@actions/core';
+import {setFailed, endGroup, info, startGroup} from '@actions/core';
 import {context} from '@actions/github';
+import {promises as fs} from 'fs';
 import process from 'process';
 import {headSHA} from './git';
 import {getInputs} from './inputs';
+import {spawnSync} from 'child_process';
 import * as proxy from './proxy';
 import * as sonarScanner from './sonar-scanner';
 import * as maven from './maven';
@@ -18,16 +20,32 @@ async function run(): Promise<void> {
     state.setIsPost();
 
     const inputs = await getInputs();
-    let sonarHost =
-      'http://sonarqube-default-sonarqube-0.sonarqube-default-sonarqube.default.svc.cluster.local:9000';
+    startGroup('Selecting sonar host');
+    let sonarHost = 'http://sonarqube:9000';
     const labels = process.env.RUNNER_LABELS;
+    info(`Found labels: ${labels}`);
     if (!labels?.includes('self-hosted') && inputs.clientCert) {
-      sonarHost = await proxy.start(
-        inputs,
-        'https://sonar.prod.tools.tradeshift.net'
+      info('Detected running on GH runners');
+      if (!inputs.caCert) {
+        throw new Error('ca-cert is required');
+      }
+      if (!inputs.clientCert) {
+        throw new Error('client-cert is required');
+      }
+      if (!inputs.clientKey) {
+        throw new Error('client-key is required');
+      }
+      await fs.writeFile('ca.crt', inputs.caCert);
+      await fs.writeFile('client.crt', inputs.clientCert);
+      await fs.writeFile('key', inputs.clientKey);
+      spawnSync(
+        'openssl pkcs12 -export -in client.crt -inkey key -CAfile ca.crt-name "sonar.prod.tools.tradeshift.net" -out sonar.p12 -passout pass:123'
       );
+      process.env['SONAR_SCANNER_OPTS'] =
+        '-Djavax.net.ssl.keyStore=sonar.p12 -Djavax.net.ssl.keyStoreType=pkcs12 -Djavax.net.ssl.keyStorePassword=123';
+      sonarHost = 'https://sonar.prod.tools.tradeshift.net';
     }
-
+    endGroup();
     const sha = await headSHA();
     const sonarArgs = args.create(inputs, sonarHost, context, sha);
 
